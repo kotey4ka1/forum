7bd2d9d2e7ade6737d6f5da982c7a2e2ae933f47
kotey4ka1 <akonkin930@gmail.com>
2026-06-14 22:25:12 +0400

    Delete resources directory

--- resources/views/forum/_edit_comment.blade.php
+++
@@ -1,9 +0,0 @@
-<form method="POST" action="{{ route('comment.update', $comment->id) }}" class="mt-2 edit-comment-form">
-    @csrf
-    @method('PUT')
-    <div class="mb-2">
-        <textarea class="form-control" name="content" rows="3" required>{{ old('content', $comment->content) }}</textarea>
-    </div>
-    <button type="submit" class="btn btn-sm btn-primary">Сохранить</button>
-    <button type="button" class="btn btn-sm btn-secondary cancel-edit">Отмена</button>
-</form>
